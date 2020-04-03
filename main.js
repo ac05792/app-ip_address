8846c5c34b0f28b902385f4ba985238f833ea0f4
ac05792 <mike.howard@centurylink.com>
2020-04-03 23:11:57 +0000

    Lab 2 part 1 of 2

--- main.js
+++ main.js
@@ -94,10 +94,6 @@ function getIpv4MappedIpv6Address(ipv4) {
   return ipv6Address;
 }
 
-//*
-  This section is used to test function and log any errors.
-  We will make several positive and negative tests.
-*/
 function main() {
   // Create some test data for getFirstIpAddress(), both valid and invalid.
   let sampleCidrs = ['172.16.10.0/24', '172.16.10.0 255.255.255.0', '172.16.10.128/25', '192.168.1.216/30'];
