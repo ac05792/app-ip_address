6e2a92387d73088d4d1a619c0d070c37a63c3192
ac05792 <mike.howard@centurylink.com>
2020-04-13 22:14:24 +0000

    Lab 4

--- main.js
+++ main.js
@@ -69,9 +69,9 @@ class IpAddress {
     // Calling the getIpv4MappedIpv6Address() to calculate the IPv4-mapped IPv6 address for the passed IPv4 address.
     let ipv4MappedIpv6 = null;
         if (!cidr.isValid()) {
-        ipv4MappedIpv6 = (`{\x1b[32m"IPv4"\x1b[37m:${firstIpAddress},\x1b[32m"IPv6"\x1b[37m:${ipv4MappedIpv6}}`)
+        ipv4MappedIpv6 = (`{"IPv4":${firstIpAddress},"IPv6":${ipv4MappedIpv6}}`)
         } else {
-        ipv4MappedIpv6 = (`{\x1b[32m"IPv4":"${firstIpAddress}"\x1b[37m,\x1b[32m"IPv6":"` + getIpv4MappedIpv6Address(firstIpAddress) +'"\x1b[37m}');
+        ipv4MappedIpv6 = (`{"IPv4":"${firstIpAddress}","IPv6":"` + getIpv4MappedIpv6Address(firstIpAddress) +'"}');
         }
     
     // Call the passed callback function.
